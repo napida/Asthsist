8db9c118d9c3b2f3436faef3d9d3b9b2c29985dc
napida <napida.cho@student.mahidol.ac.th>
2023-01-19 11:10:02 +0700

    add event button in Calendar page

--- pages/Calendar.js
+++ pages/Calendar.js
@@ -1,13 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import CalendarComponent from '../components/Calendar'
 
-const Calendar = () =>{
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <CalendarComponent />
+const Calendar = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <CalendarComponent />
+      <View style={{bottom:0, position: 'absolute', alignSelf: 'flex-end', padding:12}}>
+        <Button
+          title="+ Add event"
+          onPress={() => navigation.navigate('Tracker')}
+        />
       </View>
-    );
-  }
+    </View>
+  );
+}
 
 export default Calendar

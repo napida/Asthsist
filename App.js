d26f9deeeed90da9cca38700829491a5449076a7
napida <napida.cho@student.mahidol.ac.th>
2023-02-27 17:04:57 +0700

    Revert "Merge pull request #4 from napida/firebase-connecting"
    This reverts commit dcafaec13d4db56b446b4838b5bb9f01f0fefa5e, reversing
    changes made to 6499a42601929befb9d6d32085e8b89c26b2acd6.

--- App.js
+++ App.js
@@ -1,68 +1,16 @@
-// import { View, Text, StyleSheet  } from 'react-native'
-// import { NavigationContainer } from '@react-navigation/native';
-// import React from 'react'
-// import BottomNavBar from './components/BottomNavBar';
-// import AppNavigator from './AppNavigator'
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <BottomNavBar />
-//       {/* <AppNavigator /> */}
-//     </NavigationContainer>
-//   )
-// }
-
-// export default App
-
-import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react'
 import BottomNavBar from './components/BottomNavBar';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import Login from './pages/Login';
-import firebaseConfig from './database/firebaseDB'
-import Register from './pages/Register';
-
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const Stack = createStackNavigator();
+import AppNavigator from './AppNavigator'
 
 const App = () => {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState(null);
-
-  const onAuthStateChanged = (user) => {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  };
-
-  useEffect(() => {
-    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  }, []);
-
-  if (initializing) {
-    return null;
-  }
-
   return (
     <NavigationContainer>
-      {user ? (
-        <BottomNavBar />
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-          <Stack.Screen name="Register" component={Register} options={{headerShown:false}}/>
-        </Stack.Navigator>
-      )}
+      <BottomNavBar />
+      {/* <AppNavigator /> */}
     </NavigationContainer>
-  );
-};
+  )
+}
 
-export default App;
+export default App
